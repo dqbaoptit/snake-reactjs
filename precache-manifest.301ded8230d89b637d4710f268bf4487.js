ff081036b272c369f8c9efc8ae260a4cd3a18016
Bao <dqbaoptit@gmail.com>
2020-03-30 16:22:05 +0700

    Updates

---
+++ precache-manifest.301ded8230d89b637d4710f268bf4487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7918d8a1ba2ea85fb403bfa18551a278",
+    "url": "/snake-reactjs/index.html"
+  },
+  {
+    "revision": "9e28c5de6b3593a1067f",
+    "url": "/snake-reactjs/static/css/main.cfb84715.chunk.css"
+  },
+  {
+    "revision": "a460903110604e5a4665",
+    "url": "/snake-reactjs/static/js/2.8a45d241.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/snake-reactjs/static/js/2.8a45d241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e28c5de6b3593a1067f",
+    "url": "/snake-reactjs/static/js/main.b0bbef72.chunk.js"
+  },
+  {
+    "revision": "7cf2b081f5546e57f4f2",
+    "url": "/snake-reactjs/static/js/runtime-main.0e3a841b.js"
+  },
+  {
+    "revision": "bcf8c0fa991448a9d5cb61af4c174fad",
+    "url": "/snake-reactjs/static/media/piano.bcf8c0fa.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dqbaoptit/snake-reactjs/precache-manifest.f264e97e87990581afd03c242f244ecb.js"
+  "/snake-reactjs/precache-manifest.301ded8230d89b637d4710f268bf4487.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dqbaoptit/snake-reactjs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/snake-reactjs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
